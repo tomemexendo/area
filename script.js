d833f0531477be2bb47c035f62b5a99c69c5d045
tomemexendo <tomemexendo@gmail.com>
2026-05-01 14:44:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
 const SUPABASE_URL = "https://pxpojetrshxvtaznkxkj.supabase.co"
-const SUPABASE_ANON_KEY = "SUA_CHAVE_AQUI"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4cG9qZXRyc2h4dnRhem5reGtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1ODgxMDYsImV4cCI6MjA5MzE2NDEwNn0.ClFcL_dtAvdBQdrqZUlDi2CnhGEH_wbATrmxjJhpYYs"
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
