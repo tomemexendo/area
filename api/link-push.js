a07d5437c2e420485054cc3f24052972fadfc374
tomemexendo <tomemexendo@gmail.com>
2026-06-01 16:05:06 -0300

    Update link-push.js

--- api/link-push.js
+++ api/link-push.js
@@ -1,8 +1,17 @@
-export default function handler(req, res) {
-  console.log("API FUNCIONOU")
+import { createClient } from "@supabase/supabase-js"
 
-  return res.status(200).json({
-    ok: true,
-    message: "API rodando"
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+)
+
+export default async function handler(req, res) {
+  console.log("CHEGOU NA API")
+
+  console.log("ENV TEST:", {
+    url: process.env.SUPABASE_URL,
+    key: !!process.env.SUPABASE_ANON_KEY
   })
+
+  return res.status(200).json({ ok: true })
 }
